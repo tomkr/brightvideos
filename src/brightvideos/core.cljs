d09753e68381538fedea7e8fe3e5de00de585f09
Tom Kruijsen <tom@tomkruijsen.nl>
2016-02-03 11:46:10 +0100

    Make up and downvote buttons.

--- src/brightvideos/core.cljs
+++ src/brightvideos/core.cljs
@@ -1,6 +1,6 @@
 (ns brightvideos.core
   (:require
-    [reagent.core :as reagent :refer [atom]]
+    [reagent.core :as reagent :refer [atom cursor]]
     [devcards.core])
   (:require-macros [devcards.core :as dc :refer [defcard-rg]]))
 
@@ -10,22 +10,30 @@
 (defonce app-state (atom {:videos [{
                                     :title "test" :score 10}]}))
 
+(defn upvote-button [score]
+  [:a {:on-click #(swap! score inc)} "Up"])
+
+(defn downvote-button [score]
+  [:a {:on-click #(swap! score dec)} "Down"])
+
 ; A single video item component
 (defn video-item [video]
   [:li.video__item
-    [:a {:href (:url video) :target "_blank"} (:title video)]])
+    [upvote-button (cursor video [:score])]
+    [downvote-button (cursor video [:score])]
+    [:a {:href (:url @video) :target "_blank"} (:title @video)]])
 
 ; A list of video items
-(defn video-list []
+(defn video-list [state]
   [:div
     [:h1 "Brightvideos"]
     [:ul
-      (for [video (:videos @app-state)]
+      (for [video (:videos @state)]
         [video-item video])]])
 
 (defn main []
   (if-let [node (.getElementById js/document "app")]
-    (reagent/render-component [video-list]
+    (reagent/render-component [video-list app-state]
       (. js/document (getElementById "app")))))
 
 (main)
@@ -38,6 +46,22 @@
 
 (defcard-rg video-item
   "## A single video item"
-  [video-item @single-video-state]
+  [video-item single-video-state]
   single-video-state
   {:inspect-data true :history true })
+
+(defonce upvote-score (atom 0))
+
+(defcard-rg upvote-button
+  "## A button for upvoting"
+  [upvote-button upvote-score]
+  upvote-score
+  {:inspect-data true :history true})
+
+(defonce downvote-score (atom 20))
+
+(defcard-rg downvote-button
+  "## A button for downvoting"
+  [downvote-button downvote-score]
+  downvote-score
+  {:inspect-data true :history true})
